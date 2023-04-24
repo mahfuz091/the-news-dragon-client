a80285034eb6bc791608d1422837c872e420dce4
mahfuz091 <engmahfuz14@gmail.com>
2023-04-25 00:41:45 +0600

    rightnav added

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import logo from "../../../assets/logo.png";
 import moment from "moment";
+import { Button, Container } from "react-bootstrap";
+import Marquee from "react-fast-marquee";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 const Header = () => {
   return (
-    <div>
+    <Container>
       <div className='text-center mt-4'>
         <img src={logo} alt='' />
         <p className='text-secoundary'>
@@ -12,7 +16,31 @@ const Header = () => {
         </p>
         <p>{moment().format("dddd, MMMM D, YYYY")}</p>
       </div>
-    </div>
+      <div className='d-flex gap-4'>
+        <Button variant='danger'>Latest</Button>
+        <Marquee speed={30}>
+          I can be a React component, multiple React components, or just some
+          text.
+        </Marquee>
+      </div>
+      <Navbar collapseOnSelect expand='lg'>
+        <Container>
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse id='responsive-navbar-nav'>
+            <Nav className='mx-auto'>
+              <Nav.Link href='/'>Home</Nav.Link>
+              <Nav.Link href='#pricing'>About</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href='#deets'>Profile</Nav.Link>
+              <Nav.Link eventKey={2} href='#memes'>
+                <Button variant='secondary'>LogIn</Button>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Container>
   );
 };
 
--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,52 @@
 import React from "react";
+import { Button, ListGroup } from "react-bootstrap";
+import {
+  FaGoogle,
+  FaGithub,
+  FaTwitter,
+  FaInstagram,
+  FaFacebook,
+} from "react-icons/fa";
+import QZone from "../Qzone/Qzone";
+import bg from "../../../assets/bg.png";
 
 const RightNav = () => {
   return (
     <div>
-      <h2>RightNav</h2>
+      <h4>Login With</h4>
+      <Button variant='outline-primary'>
+        {" "}
+        <FaGoogle></FaGoogle> Login with Google
+      </Button>
+      <Button variant='outline-secondary'>
+        <FaGithub></FaGithub> Login with Github
+      </Button>
+      <div>
+        <h4 className='mt-4'>Find us on</h4>
+        <ListGroup>
+          <ListGroup.Item>
+            <FaFacebook></FaFacebook> facebook
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaTwitter></FaTwitter> twitter
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaInstagram></FaInstagram> instagram
+          </ListGroup.Item>
+        </ListGroup>
+      </div>
+      <QZone></QZone>
+      <div className='position-relative'>
+        <img src={bg} alt='' />
+        <div className='position-absolute text text-white'>
+          <h5>Create an Amazing Newspaper</h5>
+          <p>
+            Discover thousands of options, easy to customize layouts, one-click
+            to import demo and much more.
+          </p>
+          <Button variant='danger'>Latest</Button>
+        </div>
+      </div>
     </div>
   );
 };
